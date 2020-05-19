51e90a46572129e63a532708ad517068bd843a13
Kevin <knguyen3415@gmail.com>
2020-05-19 13:45:18 -0700

    Updates

---
+++ precache-manifest.797a750334780983e7bc9fca6cbf08d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a2425037f2f756cfee07d00a97ee16",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "f2a99a2fccff7d029de4",
+    "url": "/covid19-tracker/static/css/main.5b8e783f.chunk.css"
+  },
+  {
+    "revision": "111834932cebf97f3e2b",
+    "url": "/covid19-tracker/static/js/2.f93ef5ea.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.f93ef5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a99a2fccff7d029de4",
+    "url": "/covid19-tracker/static/js/main.75cb6c82.chunk.js"
+  },
+  {
+    "revision": "acd01a2ce132bb281fa5",
+    "url": "/covid19-tracker/static/js/runtime-main.3ee0972c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
