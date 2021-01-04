078ee8bdcfe533620e860679128043a618ae3bec
Kevin <knguyen3415@gmail.com>
2021-01-04 13:13:57 -0800

    Fixed chart labels

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -29,7 +29,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
           },
           {
             data: dailyData.map(({ deaths }) => deaths),
-            label: "Infected",
+            label: "Deaths",
             borderColor: "red",
             backgroundColor: "rgba(255, 0, 0, 0.5)",
             fill: true,
